1fdd9bac972d3f55143ac82556186dfe16e2b2a1
312639 <47399971+312639@users.noreply.github.com>
2019-02-11 09:33:10 -0500

    Files uploaded

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _312639IPO
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, RoutedEventArgs e)
+        {
+            double Length;
+            double Width;
+            double Area;
+            double.TryParse(txtLength.Text, out Length);
+            double.TryParse(txtLength.Text, out Width);
+
+            Area = Length * Width;
+            lblOutput.Content = "The area is:" + Area.ToString("#.00");
+        }
+    }
+}
